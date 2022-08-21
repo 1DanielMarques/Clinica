23fb05adfd9473b77cedfb519bc6737d6dc6acd0
mDanielSE <daniel.marques@edu.unifil.br>
2022-08-21 00:03:36 -0300

    validacoes_buscas

--- clinica-api/src/main/java/com/cliniconect/clinicaapi/repository/PacienteRepository.java
+++ clinica-api/src/main/java/com/cliniconect/clinicaapi/repository/PacienteRepository.java
@@ -11,8 +11,10 @@ import java.util.List;
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     @Query("select p from Paciente p where p.nome LIKE %?1% ")
     List<Paciente> buscaNome(String nome);
+
     @Query(" select p from Paciente p where p.cpf LIKE %?1% ")
-    Paciente buscaCpf(String cpf);
+    List<Paciente> buscaCpf(String cpf);
+
     @Query(" select p from Paciente p where p.email LIKE %?1% ")
     List<Paciente> buscaEmail(String email);
 }
